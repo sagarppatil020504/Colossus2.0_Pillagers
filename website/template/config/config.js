04d526d1362faf6e1d149e5df832558ceb9d4a81
sagarppatil020504 <sagarppatil020504@gmail.com>
2025-04-11 22:08:10 +0530

    completed the front end and api of the website and working on the chatbot

---
+++ website/template/config/config.js
@@ -0,0 +1,6 @@
+const pat_API_URL= "http://127.0.0.1:8000/patients";
+const staff_API_URL= "http://localhost:8000/staff_login";
+const API_URL= "http://127.0.0.1:8000";
+const vend_API_URL= "http://127.0.0.1:8000/extract_text";
+
+export {pat_API_URL, staff_API_URL, API_URL,vend_API_URL};
